acb16c35f9760263e3942431ef1c81e3a0cba6fc
Yom-gi <guillaume.pelette@protonmail.com>
2020-11-24 18:08:28 +0100

    successfully finalized AJAX request and allocated data to HTML product list page

--- src/index.js
+++ src/index.js
@@ -1,43 +1,69 @@
-//Mise a jour du nombre de produit dans l'onglet panier
-
-
-
-console.log("testchargement")
-
-
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 // On créé une fonction pour effectuer une requêt GET avec AJAX, qui retrounera une promesse
 function ajaxRequest(url) {
-  console.log("ajaxProtocol('"+url+"')");
-  var p = new Promise(function(resolved, rejected) {
-    var request = new XMLHttpRequest();
-    request.open("GET", 'http://localhost:3000/api/teddies',true);
-    request.onload = function() {
-      if (this.readyState == 4 && this.status == 200) {     // retour HTTP OK
+  console.log(`ajaxProtocol('${url}')`);
+  const p = new Promise((resolved, rejected) => {
+    // eslint-disable-next-line no-undef
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/api/teddies', true);
+    request.onload = function () {
+      if (this.readyState === 4 && this.status === 200) { // retour HTTP OK
         resolved(JSON.parse(this.responseText));
+      } else { // ereeur retour HTTP
+        rejected(`Erreur HTTP n°: ${this.status}: ${this.statusText}`);
       }
-      else {    // ereeur retour HTTP
-        rejected("Erreur HTTP n]"+this.status+" : "+this.statusText);
-      }
-    }
+    };
     request.send();
   });
   return p;
 }
 
+const urlOrinoco = 'http://localhost:3000/api/teddies';
+
 // on lance le protocole AJAX pour
-var teddies = function() {
- return ajaxRequest("http://localhost:3000/api/teddies")
-    .then(function(data) {
-    return data;
-    })
-    .catch(reason => console.log(reason));
+const teddies = function () {
+  return ajaxRequest(urlOrinoco)
+    .then((data) => data)
+    .catch((reason) => console.log(reason));
 };
 
-let teddybear = document.getElementsByClassName('card-body');
+const teddybear = document.getElementsByClassName('card-body');
+console.log(teddybear);
 
-teddies().then(function(teddies) {
-console.log(teddies);
+teddies().then((teddies) => {
+  console.log(teddies);
+  
+  let counter = 0;
+  teddies.forEach((teddy) => {
+    const article = document.createElement('article');
+    article.id = 'articleList';
+
+    const image = document.createElement('img');
+    image.id = 'teddy_image';
+    image.src = teddy.imageUrl;
+
+    const div = document.createElement('div');
+    div.id = 'teddy_div';
+
+    const name = document.createElement('h3');
+    name.textContent = teddy.name;
+    name.id = 'teddy_name';
+
+    const price = document.createElement('p');
+    price.textContent = `Prix :${teddy.price} €`;
+
+    const link = document.createElement('a');
+    link.id = 'id_link';
+    link.href = `${urlOrinoco}/${teddies._id}`;
+    link.textContent = "Voir l'ourson";
+
+    teddybear[counter].appendChild(article);
+    article.appendChild(name);
+    article.appendChild(image);
+    article.appendChild(div);
+    div.appendChild(price);
+    div.appendChild(link);
+    counter += 1;
+  });
 });
-
-
-// Affiche dynamiquement de la liste des articles grace à JS
