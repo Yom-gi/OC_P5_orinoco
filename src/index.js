a3c6c56c96b1d1e572aa73b7209b31835a0ed9e5
Yom-gi <guillaume.pelette@protonmail.com>
2020-11-26 19:26:47 +0100

    fixed AJAX protocol GET request in index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-/* eslint-disable no-console */
-/* eslint-disable no-undef */
-// On créé une fonction pour effectuer une requêt GET avec AJAX, qui retrounera une promesse
+
+
 function ajaxRequest(url) {
-  console.log(`ajaxProtocol('${url}')`);
   const p = new Promise((resolved, rejected) => {
     // eslint-disable-next-line no-undef
     const request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:3000/api/teddies', true);
+    request.open('GET', url, true);
     request.onload = function () {
       if (this.readyState === 4 && this.status === 200) { // retour HTTP OK
         resolved(JSON.parse(this.responseText));
@@ -22,40 +20,40 @@ function ajaxRequest(url) {
 const urlOrinoco = 'http://localhost:3000/api/teddies';
 
 // on lance le protocole AJAX pour
-const teddies = function () {
-  return ajaxRequest(urlOrinoco)
-    .then((data) => data)
-    .catch((reason) => console.log(reason));
+var teddies = function () {
+    return ajaxRequest(urlOrinoco)
 };
 
 const teddybear = document.getElementsByClassName('card-body');
 console.log(teddybear);
 
-teddies().then((teddies) => {
-  console.log(teddies);
+teddies().then((data) => {
+  console.log(data);
   
   let counter = 0;
-  teddies.forEach((teddy) => {
+  data.forEach(function(teddy) {
     const article = document.createElement('article');
     article.id = 'articleList';
 
     const image = document.createElement('img');
-    image.id = 'teddy_image';
+    image.id = 'teddyImage';
     image.src = teddy.imageUrl;
 
     const div = document.createElement('div');
-    div.id = 'teddy_div';
+    div.id = 'teddyDiv';
 
     const name = document.createElement('h3');
     name.textContent = teddy.name;
-    name.id = 'teddy_name';
+    name.id = "teddyName";
 
     const price = document.createElement('p');
-    price.textContent = `Prix :${teddy.price} €`;
+    price.textContent = `Prix :  ${teddy.price} €`;
+
+    let teddyId = teddy._id;
 
     const link = document.createElement('a');
-    link.id = 'id_link';
-    link.href = `${urlOrinoco}/${teddies._id}`;
+    link.id = 'linkId';
+    link.href = 'produit.html?id=' + teddyId;
     link.textContent = "Voir l'ourson";
 
     teddybear[counter].appendChild(article);
---
+++ src/product.js
@@ -0,0 +1,10 @@
+
+
+document.getElementsByClassName("mt-4 mb-3").innerHTML = "<small></small>"
+
+
+let params = new URLSearchParams(document.location.search);
+let id = params.get("teddyId");
+console.log(teddyId);
+
+teddyId
